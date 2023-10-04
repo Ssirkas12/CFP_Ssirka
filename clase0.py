0c8203523e576f2b9f6f39610634ba02c7d73b6e
Ssirkas12 <143463200+Ssirkas12@users.noreply.github.com>
2023-10-04 20:50:55 -0300

    clase python

---
+++ clase0.py
@@ -0,0 +1,38 @@
+#input sirve para mostar mensajes y que el usario ingrese datos 
+
+#los comentarios siempre entre parentesis y comillas
+#nombre = input("ingrese su nombre: ")
+#rol = input("ingrese su rol: ")
+#edad = input("ingrese su edad: ")
+#fecha = input("ingrese su fecha de nacimiento: ")
+
+#la F me permite llamar las variables entre llaves
+#print (f"hola, mi nombre es {nombre} y soy el {rol}, tengo {edad} y naci el {fecha}")
+#print ("hola, mi nombre es", nombre , "y soy el", rol , "tengo", edad, "y naci el", fecha,)
+
+#declarar 3 variables con valores ingresados por el usario (numeros) y mostrar en un PRINT el promedio de esos numeros
+
+
+#dia = int(input ("ingrese el dia de hoy: "))
+#mes = int(input ("ingrese el mes del año: "))
+#año = int(input("ingrese el año: "))
+#promedio = (dia + mes + año) / 3
+
+#print (f"el promedio del las fechas es {promedio}")
+
+#TODO: TYPE 
+#la funcion type devuelve el tipo de dato de un variable, y la estructura es "type(nombrevariable)"
+
+#entero
+num_1 = 4
+#String
+nombre = "santiago"
+#boolean
+suspendido = True
+#float
+promedio = 13.3
+
+print(type(num_1))
+print(type(nombre))
+print(type(suspendido))
+print(type(promedio))
