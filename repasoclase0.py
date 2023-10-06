b1719fa89ccf8cf4c8cda4649e71c54d9177c67a
Ssirkas12 <143463200+Ssirkas12@users.noreply.github.com>
2023-10-05 21:00:40 -0300

    repasoclase0

---
+++ repasoclase0.py
@@ -0,0 +1,77 @@
+#operaciones aritmeticas
+# num1 = 7
+# num2 = 3
+# suma = num1 + num2
+# resta = num1 - num2
+# multi = num1 * num2
+# divi = num1 / num2
+# potencia = num1 ** num2
+# divi_entera = num1 // num2
+# divi_resto = num1 % num2
+# print (f"el resultado de las operaciones de los numeros {num1} y {num2}: suma: {suma}, resta: {resta}, multiplicacion: {multi}, division: {divi}, potencia: {potencia}, division entera: {divi_entera}, division resto: {divi_resto}")
+
+
+#reglas de procedencia:
+#el orden de las operaciones es siempre de izquierda a derecha y el orden es el siguiente:
+#1_ parentesis
+#2_ potencia y raices
+#3_ multiplicacion y division 
+#4_ suma y resta
+
+#formas de imprimir mensajes por pantalla
+# print("esto es un 'texto' entre comillas")
+# print("esto es una cadena \tcon tabulacion ")
+# print("esto es una cadena \ncon salto de linea ")
+# print("""
+# esto es una cadena
+# con distintas lineas
+# """)
+
+#capturar valores por teclado
+# nombre = input('ingrese su nombre: ')
+# print (nombre)
+# print(f"el nombre ingresado fue {nombre}")
+
+#INT ==> Parsear una variable a INT
+# num1 = input("ingrese un numero")
+# num2 = input("ingrese un numero")
+# suma = num1 + num2
+# print (f"El rsultado es: {suma}")
+#forma correcta
+# num1 = int("ingrese un numero")
+# num2 = int("ingrese un numero")
+# suma = num1 + num2
+# print (f"El rsultado es: {suma}")
+
+#indexacion de las cadenas(str)
+# animal = "perro"
+# print(animal)
+# print(animal[0])
+# print(animal[1])
+# print(animal[2])
+# print(animal[3])
+# print(animal[4])
+
+# #LEN()
+# print(f"la longitud de mi animal es {len(animal)}")
+
+# fruta = "durasno"
+# # print(fruta[4])
+# # fruta[4] = "z"
+# # print(fruta)
+# fruta = fruta[0:4] + 'z' + fruta[5:]
+# print(fruta)
+
+#mostrar el siguiente mensaje correctamente 
+#pyhton es un lenguaje de programacion moderno
+# cadena1 = "moderno"
+# cadena2 = "python"
+# cadena3 = "es un lenguaje"
+# cadena4 = "de programacion"
+# print(f"{cadena2} {cadena3} {cadena4} {cadena1}")
+# cadena5 = cadena2 + " " + cadena3 + " " + cadena4 + " " + cadena1
+# print(f"{cadena5}")
+
+
+
+
